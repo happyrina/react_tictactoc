e6bf58e6c945f1bc8c02c0db26b9972b4ebb47ba
happyrina <riniversal@gmail.com>
2023-08-17 21:19:06 +0900

    tictactoc

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 import "./Board.css";
 
-export default class Board extends Component {
+const Board = () => {
 
-  renderSquare(i) {
-    return <Square value={i} />
+  
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (let index = 0; index < lines.length; index++) {
+      const [a,b,c] = lines[index];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+            }
+       }
+  return null;
+}
+
+  
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next Player ${xIsNext? 'X': '0'}`;
   }
 
-  render() {
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+
+    if(calculateWinner (newSquares) || newSquares[i]){
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : '0';
+    setSquares(newSquares);
+    setXIsNext(prev => !prev)
+  }
+
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} 
+    onClick={() => handleClick(i)}/>
+  }
+
+
     return (
       <div>
-        <div className='status'>Next Player: X, O</div>
+        <div className='status'>{status}</div>
         <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
     )
   }
-}
+
+export default Board
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,25 +1,17 @@
 import React from "react";
 import "./Square.css";
 
-export default class Square extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        value: null,
-    };
-  }
+const Square = ({onClick, value}) => {
 
-  render() {
     return (
       <button className="square" 
-        onClick={() => this.setState({value: 'X'})}
-      >
-        {this.state.value}
+        onClick = {onClick}>
+        {value}
       </button>
     )
   }
-}
 
+  export default Square
 
 
